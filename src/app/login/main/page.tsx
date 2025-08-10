34b05ddcb8b9719785fd172705d2b365aa10d364
Nicolas Pallares <nicolaspallaresdg@gmail.com>
2025-08-09 23:27:48 -0300

    feat: colaboradores

---
+++ src/app/login/main/page.tsx
@@ -0,0 +1,33 @@
+import Link from "next/link";
+import React from "react";
+import { AiOutlineLogin } from "react-icons/ai";
+import { IoIosLogIn } from "react-icons/io";
+
+const mainPage = () => {
+  return (
+    <section className="flex flex-col justify-center items-center w-screen h-screen  border-amber-600 text-primary">
+      <div className="h-80 border-red-600 flex justify-center flex-col items-center ">
+        <div className="flex justify-center items-center w-full h-15 hover:bg-complementary p-6 cursor-pointer">
+          <Link
+            className="flex flex-1 items-center mr-2"
+            href="/dashboard/main"
+          >
+            <IoIosLogIn className="mr-2" />
+            <span>Licencias</span>
+          </Link>
+        </div>
+        <div className="flex justify-center items-center w-full h-15 hover:bg-complementary p-6 cursor-pointer">
+          <Link
+            className="flex flex-1 items-center mr-2"
+            href="/dashboard/main"
+          >
+            <AiOutlineLogin className="mr-2" />
+            <span>Colaboradores</span>
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default mainPage;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { redirect } from "next/navigation";
 
 export default function HomePage() {
-  redirect("/dashboard/main");
+  redirect("/login/main");
 }
